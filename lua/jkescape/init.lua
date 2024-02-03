b6b4986dc9c8cff0ab7038e0e6a6b64b10ffd493
anflar <astonflapper@gmail.com>
2024-02-03 16:50:44 +0100

    Restructure again

--- lua/jkescape/init.lua
+++ lua/jkescape/init.lua
@@ -13,5 +13,18 @@ function M.handle_key_press(key)
   return key
 end
 
+M.setup = function()
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+      vim.b.last_key = nil
+      vim.b.last_key_time = 0
+    end,
+  })
+
+  vim.api.nvim_set_keymap('i', 'j', "v:lua.require'jk_escape'.handle_key_press('j')", {expr = true, noremap = true, silent = true})
+  vim.api.nvim_set_keymap('i', 'k', "v:lua.require'jk_escape'.handle_key_press('k')", {expr = true, noremap = true, silent = true})
+end
+
+
 return M
 
