24760cd689f9172a59a4820f0a03da0310dd3f3e
anflar <astonflapper@gmail.com>
2024-02-03 17:09:02 +0100

    Add possibility of customization

--- lua/jkescape/init.lua
+++ lua/jkescape/init.lua
@@ -1,10 +1,31 @@
 local M = {}
 
+-- Default configuration
+local config = {
+  time_window = 100, -- Time window in milliseconds for key press detection
+  map_insert = {
+    jk = '<BS><ESC>',
+    kj = '<BS><ESC>',
+  },
+  map_term = {
+    jk = '<C-\\><C-n>',
+    kj = '<Up><CR>',
+  },
+}
+
+-- Function to handle key press with time window consideration
 function M.handle_key_press(key)
   local current_time = vim.loop.now() -- Current time in milliseconds
-  if vim.b.last_key and ((current_time - vim.b.last_key_time) <= 100) then
-    if (vim.b.last_key == "j" and key == "k") or (vim.b.last_key == "k" and key == "j") then
-      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<BS><Esc>", true, true, true), "n")
+  if vim.b.last_key and ((current_time - vim.b.last_key_time) <= config.time_window) then
+    local action = ""
+    if vim.fn.mode() == 't' then -- Check if we're in Terminal mode
+      action = config.map_term[vim.b.last_key .. key] or ""
+    else
+      action = config.map_insert[vim.b.last_key .. key] or ""
+    end
+
+    if action ~= "" then
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes(action, true, true, true), "t")
       return ""
     end
   end
@@ -13,7 +34,12 @@ function M.handle_key_press(key)
   return key
 end
 
-M.setup = function()
+-- Setup function with user configuration
+function M.setup(user_config)
+  -- Extend default config with user config
+  config = vim.tbl_deep_extend("force", config, user_config or {})
+
+  -- Set up autocmd to reset last key and its time on insert enter
   vim.api.nvim_create_autocmd("InsertEnter", {
     callback = function()
       vim.b.last_key = nil
@@ -21,10 +47,17 @@ M.setup = function()
     end,
   })
 
-  vim.api.nvim_set_keymap('i', 'j', "v:lua.require'jkescape'.handle_key_press('j')", {expr = true, noremap = true, silent = true})
-  vim.api.nvim_set_keymap('i', 'k', "v:lua.require'jkescape'.handle_key_press('k')", {expr = true, noremap = true, silent = true})
+  -- Dynamic key mappings based on configuration, for both insert and terminal modes
+  local modes = {'i', 't'}
+  for _, mode in ipairs(modes) do
+    for keys, _ in pairs(mode == 'i' and config.map_insert or config.map_term) do
+      for i = 1, #keys do
+        local key = keys:sub(i,i)
+        vim.api.nvim_set_keymap(mode, key, string.format("v:lua.require'jkescape'.handle_key_press('%s')", key), {expr = true, noremap = true, silent = true})
+      end
+    end
+  end
 end
-
 
 return M
 
